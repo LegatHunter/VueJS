57b057f18cca384280c1b92bff1d691c172fd96f
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-09-18 00:21:56 +0300

    first commit

---
+++ Vue 3 2021 Владилен Минин/Sem_02/App.js
@@ -0,0 +1,16 @@
+const App = {
+    data() {
+        return {
+            tittle: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+        }
+    },
+    methods: {
+        inputChange(event) {
+console.log('inputChange', event)
+        }
+    }
+}
+
+const app = Vue.createApp(App).mount('#app')

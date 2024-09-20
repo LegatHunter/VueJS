2dfd447729a0d1cdcf3af088f3ecc1c33844f0c1
Nikita Prebyshevskiy <132432201+LegatHunter@users.noreply.github.com>
2024-09-21 00:48:58 +0300

    Sem_03 1/2 done!

--- Vue 3 2021 Владилен Минин/Sem_02/App.js
+++ Vue 3 2021 Владилен Минин/Sem_02/App.js
@@ -1,16 +1,43 @@
 const App = {
-  data() {
-    return {
-      tittle: "Список заметок",
-      placeholderString: "Введите название заметки",
-      inputValue: "",
-    };
-  },
-  methods: {
-    inputChange(event) {
-      console.log("inputChange", event);
+    data() {
+        return {
+            tittle: "Список заметок",
+            placeholderString: "Введите название заметки",
+            inputValue: "",
+            notes: ["Заметка 1", "Заметка 2", "Заметка 3"],
+        };
     },
-  },
+    methods: {
+
+        addNewNote() {
+            if (this.inputValue === "") {
+                return;
+            }
+            this.notes.push(this.inputValue);
+            this.inputValue = "";
+        },
+        toUpperCase(item) {
+            return item.toUpperCase();
+        },
+        removeNote(i) {
+            this.notes.splice(i, 1);
+            },
+        removeNotesAll() {
+            this.notes = []
+        }
+    },
+    computed:{
+        doubleCountComputed(){
+            return this.notes.length * 2;
+        },
+    },
+    watch: {
+        inputValue(value){
+            if (value.length> 10){
+                this.inputValue = ''
+            }
+        }
+    }
 };
 
 const app = Vue.createApp(App).mount("#app");

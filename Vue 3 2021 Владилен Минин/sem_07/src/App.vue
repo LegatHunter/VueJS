b2cbcf9b7021af8405895633eff98c453b4b131b
Nikita Prebyshevskiy <132432201+LegatHunter@users.noreply.github.com>
2024-09-30 21:58:35 +0300

    Sem_07

--- Vue 3 2021 Владилен Минин/sem_07/src/App.vue
+++ Vue 3 2021 Владилен Минин/sem_07/src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container">
-    <form class="card" @submit="submitHandler">
+    <form class="card" @submit.prevent="submitHandler">
       <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control">
-        <label for="name">Как тебя зовут?</label>
-        <input type="text" id="name" placeholder="Введи имя" />
-      </div>
-
+      <AppInput
+        placeholder="Введите имя"
+        :error="errors.name"
+        label="Как тебя зовут?"
+        v-model="name"
+      >
+      </AppInput>
       <div class="form-control">
         <label for="age">Выбери возраст</label>
-        <input type="number" id="age" value="20" />
+        <input type="number" id="age" max="70" v-model.number="age" />
       </div>
 
       <div class="form-control">
         <label for="city">Твой город</label>
-        <select id="city">
+        <select id="city" v-model="city">
           <option value="spb">Санкт-Петербург</option>
           <option value="msk">Москва</option>
           <option value="kzn">Казань</option>
@@ -25,34 +27,107 @@
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input type="radio" name="trip" /> Да</label>
+          <label
+            ><input type="radio" name="trip" v-model="relocate" value="yes" />
+            Да</label
+          >
         </div>
 
         <div class="checkbox">
-          <label><input type="radio" name="trip" /> Нет</label>
+          <label
+            ><input type="radio" name="trip" v-model="relocate" value="no" />
+            Нет</label
+          >
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox" /> Vuex</label>
+          <label
+            ><input type="checkbox" value="vuex" v-model="vue" name="vue" />
+            Vuex</label
+          >
         </div>
         <div class="checkbox">
-          <label><input type="checkbox" /> Vue CLI</label>
+          <label
+            ><input type="checkbox" value="Vue CLI" v-model="vue" name="vue" />
+            Vue CLI</label
+          >
         </div>
         <div class="checkbox">
-          <label><input type="checkbox" /> Vue Router</label>
+          <label
+            ><input
+              type="checkbox"
+              value="Vue Router"
+              v-model="vue"
+              name="vue"
+            />
+            Vue Router</label
+          >
         </div>
       </div>
-
+      <div class="form-checkbox">
+        <span class="label">Правила нашей компании</span>
+        <label
+          ><input type="checkbox" value="soglasen" v-model="soglasen"
+        /></label>
+        Согласен
+      </div>
       <button type="submit" class="btn primary">Отправить</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import AppInput from "@/components/AppInput.vue";
+export default {
+  data() {
+    return {
+      name: "",
+      age: 18,
+      city: "spb",
+      relocate: "yes",
+      vue: [],
+      soglasen: false,
+      errors: {
+        name: null,
+      },
+    };
+  },
+  components: { AppInput },
+  methods: {
+    formIsValid() {
+      let isValid = true;
+      if (this.name.length === 0) {
+        this.errors.name = "Имя неможет быть пустым";
+        isValid = false;
+      } else {
+        this.errors.name = null;
+      }
+      return isValid;
+    },
+    submitHandler() {
+      if (this.formIsValid()) {
+        console.group("form data");
+        console.log("Name", this.name);
+        console.log("Age", this.age);
+        console.log("City", this.city);
+        console.log("Tokyo", this.relocate);
+        console.log("Vue", this.vue);
+        console.log("Soglasen", this.soglasen);
+        console.groupEnd();
+      }
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+.form-control small {
+  color: #e53935;
+}
+.form-control.invalid input {
+  border-color: #e53935;
+}
+</style>
